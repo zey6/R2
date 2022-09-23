e48a746b8ef5470adf185c0814b504858cd5fef7
Isaiah Leapheart <ijleapheart19@students.desu.edu>
2022-09-23 10:17:37 -0400

    i passed

--- crypto.py
+++ crypto.py
@@ -7,7 +7,13 @@
 # ***************************************************
 
 def crypto(filename, cypher):
-    pass # TODO please replace this line with your code
+        with open(filename, 'r') as fh:
+            for line in fh:
+                eLine = ''
+                for ch in line:
+                    eLine += cypher(ch)
+        with open(filename, 'w') as fhenc:
+                fhenc.write(eLine)
 
 # DO NOT touch the lines below
 
